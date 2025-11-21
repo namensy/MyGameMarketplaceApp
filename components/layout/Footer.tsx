6051285a6328be0d8d9739bc1773b786552162e2
namensy <Apiset1012@hotmail.com>
2025-11-21 17:53:22 +0700

    Update dependencies, enhance global styles, and improve layout components: added new packages for UI enhancements, updated CSS variables for better theming, and refined Navbar and Footer components for improved presentation.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <footer className="border-t border-slate-800 bg-slate-950" >
-      <div className="max-w-6xl mx-auto px-4 py-6 text-xs text-slate-400 flex flex-col sm:flex-row sm:items-center sm: justify-between gap-2">
-        Hello</div>
+      <div className="max-w-6xl mx-auto px-4 py-6 text-xs text-slate-400">
+      Copyright © <span className="font-bold">Namensy</span> | All Rights Reserved
+      </div>
     </footer>
 
   )
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Button } from "../ui/button"
 
 const Navbar = () => {
   return (
@@ -7,9 +8,10 @@ const Navbar = () => {
         <div className="flex items-center justify-center gap-4">
           <Image src="/images/king.png" width={36} height={36} alt="King Logo" />
           <span className="font-semibold">Orifuke Games</span>
+          <Button className="bg-purple-200 text-black hover:bg-purple-400 uppercase" size="sm" aria-label="Sign In">Store</Button>
         </div>
         <div>
-          <button type="button">Sign in</button>
+          <Button className="bg-purple-200 text-black hover:bg-purple-400" size="sm" aria-label="Sign In">Sign In</Button>
         </div>
       </nav>
     </header>
